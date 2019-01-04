35128887d63e124a2c33feeb5429974670aa3219
Biswajit Saha <gbjsolution@gmail.com>
2019-01-04 20:51:48 +0530

    Updates

---
+++ precache-manifest.15f07d031938d0364e6a4906dcd64591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5429573ecf2e83ad0f91",
+    "url": "/quiz-master/static/js/runtime~main.5429573e.js"
+  },
+  {
+    "revision": "711c1f761b9871f028e2",
+    "url": "/quiz-master/static/js/main.711c1f76.chunk.js"
+  },
+  {
+    "revision": "b3fa1b2f48aee491df12",
+    "url": "/quiz-master/static/js/1.b3fa1b2f.chunk.js"
+  },
+  {
+    "revision": "711c1f761b9871f028e2",
+    "url": "/quiz-master/static/css/main.818fd2c7.chunk.css"
+  },
+  {
+    "revision": "0c7f70dd077b71212456a57f972f9a07",
+    "url": "/quiz-master/index.html"
+  }
+];
